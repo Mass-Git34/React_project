3b8dfe4d4845e3d4192e4bef1c806b7dc69a7138
MasQuantumCoder-34 <mdmass472@gmail.com>
2025-01-25 15:37:43 +0530

    added content

--- src/pages/Task3.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ComingSoon from "../components/ComingSoon";
-
-const Task3 = () => {
-  return <ComingSoon message="Coming Soon Page..." />;
-};
-
-export default Task3;
